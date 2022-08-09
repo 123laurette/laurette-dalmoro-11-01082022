589b361f06fc3ba00279f74da4c728d8bfe57fc7
laurette <dalmorolaurette@orange.fr>
2022-08-09 18:52:25 +0200

    dropdown court

--- src/Component/Dropdown/DropdownCourt.jsx
+++ src/Component/Dropdown/DropdownCourt.jsx
@@ -1,19 +1,53 @@
 import React from "react"
 import arrowclose from "../../assets/arrow_close.png"
+import arrowopen from "../../assets/arrow_open.png"
 import "./DropdownCourt.css"
+import { useState } from "react"
 
 
+function Dropdown({ title, text, extraClass }) {
+    const [dropdownOpened, setDropdown] = useState(false)
 
-function DropdownCourtClose({contenu}) {
-    return (
+    return dropdownOpened ? (
+        <div className={extraClass ? "dropdown " + extraClass : "dropdown"}>
         <div className="titre-description">
-            <h4>{contenu}</h4>
-            <img src={arrowclose} alt="" />
+            <h3>{title}</h3>
+            <img
+            src={arrowclose}
+            alt="Cacher contenu"
+            onClick={() => setDropdown(false)}
+            />
+        </div>
+        {typeof text === "object" ? convertArray(text) : <p>{text}</p>}
+        </div>
+    ) : (
+        <div className="dropdown">
+        <div className="titre-description">
+            <h3>{title}</h3>
+            <img
+            src={arrowopen}
+            alt="Afficher contenu"
+            onClick={() => setDropdown(true)}
+            />
+        </div>
         </div>
     )
 }
 
-//modifier cette fonction pour qu'elle soit générique.
-//elle doit servir pour description et équipement
+function convertArray(arr) {
+    return (
+        <ul>
+        {arr.map((equipment) => (
+            <li>{equipment}</li>
+        ))}
+        </ul>
+    )
+}
 
-export default DropdownCourtClose
+export default Dropdown
+
+
+
+
+
+
